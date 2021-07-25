d9573f6507b5c7b9bb2d6a92d883bb8394e97e52
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-07-25 17:30:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -20,8 +20,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.packname = 'Mok'
+global.author = 'Li'
 
 global.multiplier = 69 // The higher, The harder levelup
 
